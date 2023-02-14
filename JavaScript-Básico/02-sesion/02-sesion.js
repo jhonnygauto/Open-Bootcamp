76e1a756f358bfe7bc8a8dc21abb5ed15725faa0
Jhonny Gauto <110848998+jhonnygauto@users.noreply.github.com>
2023-02-14 15:29:47 -0300

    Add files via upload

---
+++ JavaScript-Básico/02-sesion/02-sesion.js
@@ -0,0 +1,10 @@
+const nombre = "Jhonny";
+const edad = 30;
+const eresDesarrollador = true;
+const fechaNacimiento = new Date("4, 2, 1992");
+const libroFavorito = {
+    titulo: "Harry Potter y la Piedra Filosofal",
+    autor: "J.K. Rowling",
+    fecha: new Date("6, 27, 1997 "),
+    url: "https://www.amazon.com/-/es/J-K-Rowling/dp/8478884459"
+}
