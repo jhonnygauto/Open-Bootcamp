64a7de56c3b53ca7b2bc1917ab5917dfa4560d9a
Jhonny Gauto <110848998+jhonnygauto@users.noreply.github.com>
2023-02-16 21:39:34 -0300

    Add files via upload

---
+++ JavaScript-Básico/05-sesion/numeros-javascript.js
@@ -0,0 +1,17 @@
+let alturaCms = 168;
+console.log(typeof alturaCms);
+
+let alturaMts = 1.69;
+console.log(typeof alturaMts);
+
+let peso = 58.653;
+console.log(peso);
+
+let alturaMtsRedArriba = alturaMts.toFixed();
+console.log(alturaMtsRedArriba);
+
+let pesoRedAbajo = Math.floor(peso);
+console.log(pesoRedAbajo);
+
+let maximoValor = (Number.MAX_VALUE + 1) === (Number.MAX_VALUE);
+console.log(maximoValor);
